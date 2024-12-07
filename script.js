891bd9dde23beb50470446917e8ea40e8124bb38
Praveen kumar <168343716+Praveenkushinpi@users.noreply.github.com>
2024-12-07 21:39:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+const synth = window.speechSynthesis;
 
+const voiceSelect = document.getElementById('voice-select');
+
+let voices = [];
+
+function loadVoices() {
+
+    voices = synth.getVoices();
+
+    voiceSelect.innerHTML = ''; 
+    voices.forEach((voice, index) => {
+
+        const option = document.createElement('option');
+
+        option.textContent = `${voice.name} (${voice.lang})`;
+
+        option.value = index;
+
+        voiceSelect.appendChild(option);
+
+    });
+
+}
+
+
+
+if (synth.onvoiceschanged !== undefined) {
+
+    synth.onvoiceschanged = loadVoices;
+
+    loadVoices(); 
+
+}
+
+document.getElementById('speak-button').addEventListener('click', function() {
+
+    const text = document.getElementById('text-input').value;
+
+    if (text.trim() !== "") {
+
+        const utterance = new SpeechSynthesisUtterance(text);
+
+        const selectedVoiceIndex = voiceSelect.value;
+
+        utterance.voice = voices[selectedVoiceIndex]; 
+
+        synth.speak(utterance); 
+    } else {
+
+        alert('Please enter some text to speak.');
+
+    }
+
+});
